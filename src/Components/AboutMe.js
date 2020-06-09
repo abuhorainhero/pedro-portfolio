22902ff6115ba068b76ecf20f2ab0603ff3a7381
Pedro Machado <machadop1407@gmail.com>
2020-06-08 21:00:33 -0300

    changed images and changed skills set

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -17,18 +17,17 @@ const AboutMe = () => {
                         <div className="col-lg-5">
                             <ul>
                                 <b>Front-End</b>
-                                <li>HTML5</li>
-                                <li>CSS3</li>
-                                <li>JavaScript</li>
-                                <li>ReactJS</li>
+                                <li>HTML / CSS / JavaScript </li>
+                                <li>ReactJS / React Native</li>
                                 <li>Jquery</li>
+                                <li>Bootstrap 4</li>
                             </ul>
                         </div>
                         <div className="col-lg-5">
                             <ul> <b>Back-End</b>
-                                <li>NodeJS</li>
-                                <li>ExpressJS</li>
+                                <li>NodeJS / ExpressJS</li>
                                 <li>MySQL</li>
+                                <li>MongoDB (No SQL) </li>
                                 <li>Google Cloud (API, Storage...)</li>
                                 <li>AWS</li>
                             </ul>
@@ -37,10 +36,12 @@ const AboutMe = () => {
                 </div>
 
                 <div className="col-lg-3">
-                    <h1>Other Habilities</h1>
+                    <h1>Programming / Design</h1>
                     <hr />
-                    <ul> <li>Unity Game Dev in C#</li>
-                        <li>Mobile Dev with JAVA</li>
+                    <ul> 
+                        <li>Unity Game Dev in C#</li>
+                        <li>Java Object Origented Programming</li>
+                        <li>Great at Data Structures and Algorithms</li>
                         <li>PhotoShop CC</li>
                         <li>Blender 3D Modelling</li>
                     </ul>
